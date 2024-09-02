83deebf809bb204969b6e27e9d2f9c2b94525894
DevDimon <devdimonpok@gmail.com>
2024-09-02 17:00:04 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function toReadable (number) {
-  
+    digits = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten",
+     "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"]
+
+    decades = ["", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"]
+
+    indDigits = number % 10
+    indDecades = Math.trunc(number%100/10)
+    indHundreds = Math.trunc(number / 100)
+    result = ""
+    
+    if (number >= 0 && number <= 19) return digits[number]
+
+    if (indHundreds > 0) result = result + digits[indHundreds] + " hundred"
+    if (number % 100 >= 1 && number % 100 <= 19) {
+        result = result + " " + digits[number % 100]
+        return result
+    }
+    if (indDecades > 0) result = result + " " + decades[indDecades]
+    if (indDigits > 0) result = result + " " + digits[indDigits]
+    return result.trim()
 }
